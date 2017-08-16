1ab9dae76c4f42ca31f0031be02e71072beb587d
Rob Levine <rob@roblevine.co.uk>
2017-08-16 23:01:16 +0100

    Add aggregate

---
+++ Rob.ValuationMonitoring.Calculation/ValuationLineAggregate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventFlow.Aggregates;
+
+namespace Rob.ValuationMonitoring.Calculation
+{
+    public class ValuationLineAggregate : AggregateRoot<ValuationLineAggregate, ValuationLineId>
+    {
+        public ValuationLineAggregate(ValuationLineId id) : base(id)
+        {
+        }
+    }
+}
---
+++ Rob.ValuationMonitoring.Calculation/ValuationLineId.cs
@@ -0,0 +1,14 @@
+﻿using EventFlow.Core;
+using EventFlow.ValueObjects;
+using Newtonsoft.Json;
+
+namespace Rob.ValuationMonitoring.Calculation
+{
+    [JsonConverter(typeof(SingleValueObjectConverter))]
+    public class ValuationLineId : SingleValueObject<string>, IIdentity
+    {
+        public ValuationLineId(string value) : base(value)
+        {
+        }
+    }
+}

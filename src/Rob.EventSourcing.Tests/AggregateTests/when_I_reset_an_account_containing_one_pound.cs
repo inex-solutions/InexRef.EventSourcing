29066847c0051e6bb4cb55363ba9dedf3f207bb3
Rob Levine <rob@roblevine.co.uk>
2017-09-07 22:28:58 +0100

    State license is MIT in file header

--- src/Rob.EventSourcing.Tests/AggregateTests/when_I_reset_an_account_containing_one_pound.cs
+++ src/Rob.EventSourcing.Tests/AggregateTests/when_I_reset_an_account_containing_one_pound.cs
@@ -1,4 +1,6 @@
 #region Copyright & License
+// The MIT License (MIT)
+// 
 // Copyright 2017 INEX Solutions Ltd
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software
--- src/Rob.EventSourcing.Tests/IntegrationTests/BalanceUpdatedEvent.cs
+++ src/Rob.EventSourcing.Tests/IntegrationTests/BalanceUpdatedEvent.cs
@@ -1,4 +1,6 @@
 ﻿#region Copyright & License
+// The MIT License (MIT)
+// 
 // Copyright 2017 INEX Solutions Ltd
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software

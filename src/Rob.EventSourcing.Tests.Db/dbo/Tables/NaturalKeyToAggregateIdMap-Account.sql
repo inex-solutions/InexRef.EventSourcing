43b98d7aa105bd0f9332483c32d2a201bab3e747
Rob Levine <rob@roblevine.co.uk>
2017-09-23 18:12:59 +0100

    Update dacpac - missed from sql map work

---
+++ src/Rob.EventSourcing.Tests.Db/dbo/Tables/NaturalKeyToAggregateIdMap-Account.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[NaturalKeyToAggregateIdMap-Account] (
+    [NaturalKey]  NVARCHAR (50)    NOT NULL,
+    [AggregateId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_NaturalKeyToAggregateIdMap-Account] PRIMARY KEY CLUSTERED ([NaturalKey] ASC)
+);
+

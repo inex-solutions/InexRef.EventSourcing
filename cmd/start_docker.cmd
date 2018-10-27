6f1f787a578905fef3ad0c12bd33561fca0c6e1f
Rob Levine <rob@roblevine.co.uk>
2018-10-27 22:37:28 +0100

    Add persistent sql volume

--- cmd/start_docker.cmd
+++ cmd/start_docker.cmd
@@ -1,1 +1,3 @@
-docker run -p 1433:1433 -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=EventSourceT3st!" --name mssql microsoft/mssql-server-linux:2017-latest
+docker run -p 1433:1433 -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=EventSourceT3st!" -v sqlvolume:/var/opt/mssql --name mssql microsoft/mssql-server-linux:2017-latest
+
+

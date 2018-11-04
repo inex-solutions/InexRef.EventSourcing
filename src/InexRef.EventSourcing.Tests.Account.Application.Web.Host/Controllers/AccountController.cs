2f5762ae17dbd95164b35df286983760bdb0486e
Rob Levine <rob@roblevine.co.uk>
2018-11-04 11:59:34 +0000

    Add shell web api for account domain

---
+++ src/InexRef.EventSourcing.Tests.Account.Application.Web.Host/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using InexRef.EventSourcing.Tests.Account.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InexRef.EventSourcing.Tests.Account.Application.Web.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<AccountId> Get()
+        {
+            return Enumerable.Empty<AccountId>();
+        }
+    }
+}

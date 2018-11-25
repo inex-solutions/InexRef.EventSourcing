31d08706d63591fdff3b81748660389eca51577e
Rob Levine <rob@roblevine.co.uk>
2018-11-25 07:12:47 +0000

    Add event store persistence project

---
+++ src/InexRef.EventSourcing.Persistence.EventStore/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace InexRef.EventSourcing.Persistence.EventStore2
+{
+    public class Class1
+    {
+        private EventStore.ClientAPI.ClusterSettings settings;
+    }
+}

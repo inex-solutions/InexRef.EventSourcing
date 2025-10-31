3905f2351b45da8b7fe091c9bdb12965015d8e0b
Rob Levine <rob@roblevine.co.uk>
2025-10-31 14:53:36 +0000

    Move assembly metadata from AssemblyInfo.cs to .csproj
    Assembly metadata attributes such as title, product, copyright, and version information were removed from AssemblyInfo.cs and added to the Rob.EventSourcing.Sql.csproj file. This modernizes the project by centralizing assembly metadata in the project file, aligning with .NET SDK-style project conventions. No functional code changes were made.

--- src/Rob.EventSourcing.Sql/Properties/AssemblyInfo.cs
+++ src/Rob.EventSourcing.Sql/Properties/AssemblyInfo.cs
@@ -21,16 +21,6 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Rob.EventSourcing.Sql")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rob.EventSourcing.Sql")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -41,16 +31,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("32b7b270-01c3-43c9-aea5-795b414231a7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
